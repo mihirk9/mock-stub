e46e8272cdebfa9d70aa4b94399abe7523a04059
Mihir Kayastha <mihir.kayastha@medly.com>
2021-08-19 07:47:31 +0530

    refactor: removed mock classes for BankingService & Printer

--- src/test/kotlin/com/example/FakeBankingService.kt
+++
@@ -1,9 +0,0 @@
-package com.example
-
-import java.lang.Exception
-
-class FakeBankingService(private val shouldTransactionSucceed: Boolean) : BankingService {
-    override fun withdraw(amount: Int) {
-        if (!shouldTransactionSucceed) throw Exception()
-    }
-}
